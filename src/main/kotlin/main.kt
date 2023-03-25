bfefea698d1263043eb71617b0b75421fda68d98
Luiz Marangoni <lzmarangoni@gmail.com>
2023-03-24 21:13:50 -0300

    projeto finalizado com as funções de deposito, transferencia e ssaque

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -9,7 +9,7 @@ fun main(){
     contaLuiz.deposita( -1.00)
     contaLuiz.sacar(50.00)
 
-    var contaLeticia = Conta("Leticia", 1002)
+    var contaLeticia = Conta(titular="Leticia", numeroDaConta = 1002)
 
     if(contaLuiz.tranferencia(50.00, contaLeticia)){
         println("concluido")
@@ -21,17 +21,14 @@ fun main(){
 }
 
 
-class Conta(var titular:String, var numeroDaConta: Int){
- 
+class Conta(
+    var titular:String,
+    var numeroDaConta: Int){
+
     var saldo = 0.0
         private set
 
-//    constructor(titular:String, numeroDaConta: Int){
-//        this.titular = titular
-//        this.numeroDaConta = numeroDaConta
-//    }
-
-        fun deposita(valor: Double){
+     fun deposita(valor: Double){
             if(valor > 0){
                 this.saldo += valor
                 println("foi depositado o valor de ${valor} para a conta do ${titular}, seu novo saldo é de ${saldo}")
@@ -61,47 +58,5 @@ class Conta(var titular:String, var numeroDaConta: Int){
 
 }
 
-//fun testaCopiaseReferencias(){
-//    val numeroX = 10
-//    var numeroY = numeroX
-//    numeroY++
-//    println(numeroY)
-//
-//    val contaJoao = Conta()
-//    contaJoao.titular = "Luiz"
-//
-//    val contaMaria = contaJoao
-//    contaMaria.titular ="João"
-//
-//    println(contaJoao.titular)
-//    println(contaMaria.titular)
-//}
 
-//fun testandoRepeticoes(){
-//    var i =0
-//    while (i < 5) {
-//        val titular: String = "Luiz $i"
-//        val contaCorrente: Int = 1234 + i
-//        var saldo: Double = 0.0 + i
-//
-//        println("Titular: $titular")
-//        println("Conta Corrente: $contaCorrente")
-//        println("Saldo: $saldo")
-//        testandoCondicoes(saldo)
-//        println()
-//        i++
-//
-//    }
-//
-//
-//}
-//
-//fun testandoCondicoes(saldo: Double){
-//    when {
-//        saldo > 0.0 -> println("saldo positivo")
-//        saldo == 0.0 -> println("sem saldo")
-//        else -> println("conta negativa")
-//    }
-//
-//}
 
