e4c0c53907930ebd62521547c8d91faede92b222
Luiz Marangoni <lzmarangoni@gmail.com>
2023-04-11 20:44:34 -0300

    Salvo os primeiros passos explicando a sintaxe da linguagem Kotlin

--- src/main/kotlin/Funcionario.kt
+++ src/main/kotlin/Funcionario.kt
@@ -1,10 +1,8 @@
-class Funcionario(val nome:String,
+open class Funcionario(val nome:String,
                   val matricula:Int,
                   val salario:Double) {
 
     fun bonificacao(): Double {
         return salario * 0.1
     }
-
-
 }
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -12,6 +12,24 @@ fun main(){
     println("Salário: ${funcionario1.salario}")
     println("O valor da sua bonificação é ${funcionario1.bonificacao()}")
 
+    var gerente1 = Gerente(
+        "Leticia",
+        2,
+        3000.00,
+        1234)
+
+    var diretor = Diretor(
+        "Roberto",
+        150,
+        20000.0,
+        1234,
+        200.0
+    )
+    println("Funcionario: ${diretor.nome}")
+    println("Matricula: ${diretor.matricula}")
+    println("Salário: ${diretor.salario}")
+    println("plr: ${diretor.plr}")
+    println("senha: ${diretor.autentica(1234)}")
 }
 
 
